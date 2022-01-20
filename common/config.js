a91d330dabebcc034c6c9dcb8f82e3a6009ae91b
Randolph Chin <randychin@163.com>
2022-01-20 17:10:12 +0800

    Update config.js

--- common/config.js
+++ common/config.js
@@ -2,14 +2,14 @@ const CONFIG = {
 	// 开发环境配置
 	development: {
 		//baseUrl: 'http://127.0.0.1:8100/wechat',
-		baseUrl: 'https://hkwis.com/wechat',
+		baseUrl: 'https://wwe.com/wechat',
 		//websocket: 'ws://127.0.0.1:8100/wechat/webSocket/wechat'
-		websocket: 'wss://hkwis.com/wechat/webSocket/wechat'
+		websocket: 'wss://wwe.com/wechat/webSocket/wechat'
 	},
 	// 生产环境配置
 	production: {
-		baseUrl: 'https://hkwis.com/wechat',
-		websocket: 'wss://hkwis.com/wechat/webSocket/wechat'
+		baseUrl: 'https://wwe.com/wechat',
+		websocket: 'wss://wwe.com/wechat/webSocket/wechat'
 	}
 }
 export default CONFIG[process.env.NODE_ENV];
